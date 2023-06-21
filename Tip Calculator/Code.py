9d92dda7f8850671a799ab4772e799938437baee
Shikhar Mishra <shikharmishra108@gmail.com>
2023-06-21 11:11:57 +0530

    Create Code.py
    Calculates how much tip everyone has to pay.

---
+++ Tip Calculator/Code.py
@@ -0,0 +1,8 @@
+print("Welcome to the tip calculator")
+bill = float(input("What was the total bill ? $"))
+cent = int(input("What percentage tip would you like to give ? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+tip = bill * (cent/100)
+amount = (bill/people)+(tip/people)
+a = "{:.2f}".format(amount)
+print(f"Each person should pay: ${a}")
